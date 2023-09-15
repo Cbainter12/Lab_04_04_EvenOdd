874bbd4b61853eda566bbc1121cfb5adf4f955e0
Carter <carterleebainter@gmail.com>
2023-09-15 17:38:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int numToExamine = 3;
+
+        if (numToExamine % 2 ==0)
+            System.out.println("Your number is even");
+        else
+            System.out.println("Your number is odd");
+
+    }
+}
